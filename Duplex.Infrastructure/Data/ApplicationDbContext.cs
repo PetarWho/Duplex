e77007c52a0e57350860c44acac39367c9d94eb3
Roy <92612927+PetarWho@users.noreply.github.com>
2022-11-06 14:57:29 +0200

    Create Models

--- Duplex.Infrastructure/Data/ApplicationDbContext.cs
+++ Duplex.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Duplex.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Duplex.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Prize> Prizes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
